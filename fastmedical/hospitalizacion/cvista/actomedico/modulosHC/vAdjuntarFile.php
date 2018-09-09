122e318a7460f5fa2d44876fbce6f5ee8b2498db
medifacil <medifacil.uni@gmail.com>
2018-09-09 16:54:04 -0500

    en netbeanse

--- fastmedical/hospitalizacion/cvista/actomedico/modulosHC/vAdjuntarFile.php
+++ fastmedical/hospitalizacion/cvista/actomedico/modulosHC/vAdjuntarFile.php
@@ -1,8 +1,4 @@
-
-
-
-
-<div id="Div_AdjuntarFile" style="width:100%;float: left   "  >
+<div id="Div_AdjuntarFile" style="width:100%;float: left   ">
     <div id="Div_AdjuntarFileEncabezado" class="titleform" style="cursor: pointer;" onclick="javascript:abrirDiv('Div_AdjuntarFileCuerpo');">
         <table style="height: auto;width: 100%" class="accordion_title">
             <tr align="center">
@@ -10,33 +6,50 @@
                     <h1>Adjuntar File o Imagen</h1>
                 </td>
                 <td style="width:3%">
-                    <img id="Div_AdjuntarFileCuerpoicono" src='../../../../fastmedical_front/imagen/icono/plegar.png' title='plegar' alt=""/>
+                    <img id="Div_AdjuntarFileCuerpoicono" src='../../../../fastmedical_front/imagen/icono/plegar.png'
+                        title='plegar' alt="" />
                 </td>
             </tr>
         </table>
     </div>
 
 
-    
+
+
+
     <div id="Div_AdjuntarFileCuerpo" style="width: 95%;height: auto;display:block">
 
+        <fieldset>
+            <legend>Adjuntar Imagen</legend>
+
+            <table width="100%" border="0">
+                <tr>
+                    <td>
+                    <div style>
+
+                    </div>
+                    </td>
+                    
+                </tr>
+                
+            </table>
+            <input type="file" id="campoarchivo">
+
+            <div id="controles">
+                <button id="pausar">Pausar</button>
+                <button id="reanudar">Reanudar</button>
+                <button id="cancelar">Cancelar</button>
+            </div>
+
+            <div id="mensaje"></div>
+
+            <div id="archivo">
+                Archivo subido: <a href="" id="enlace">Click para ver</a>
+            </div>
+        </fieldset>
 
 
 
-
-        <input type="file" id="campoarchivo">
-
-        <div id="controles">
-        <button id="pausar">Pausar</button>
-        <button id="reanudar">Reanudar</button>
-        <button id="cancelar">Cancelar</button>
-        </div>
-
-        <div id="mensaje"></div>
-
-        <div id="archivo">
-        Archivo subido: <a href="" id="enlace">Click para ver</a>
-        </div>
 
 
     </div>
