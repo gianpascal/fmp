299b3a7b5ef31209e6f1ea4bde08554c5a091828
medifacil <medifacil.uni@gmail.com>
2018-12-02 11:22:43 -0500

    corrige acto medico v2

--- fastmedical/hospitalizacion/cdatos/DActoMedico.php
+++ fastmedical/hospitalizacion/cdatos/DActoMedico.php
@@ -2410,9 +2410,6 @@ class DActoMedico extends Adophp {
         parent::SetParameterSP("$3", $idEstadoDesarrollo);
         parent::SetParameterSP("$4", $iCodigoProgramacion);
         $resultado = parent::executeSPArrayX();
-        echo "<br>di";
-        var_dump($resultado);
-        echo "<br>di";
         parent::Close();
         return $resultado;
     }
